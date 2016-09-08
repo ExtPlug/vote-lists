776e03cabcd51daebba220cade9ce1b6264b5f15
René Kooi <renee@kooi.me>
2016-09-08 13:28:37 +0200

    es20something imports

--- src/VoteListView.js
+++ src/VoteListView.js
@@ -1,33 +1,29 @@
-define(function (require, exports, module) {
+import { throttle } from 'underscore'
+import UserListView from 'plug/views/rooms/users/RoomUsersListView'
+import UserRowView from 'plug/views/rooms/users/RoomUserRowView'
+import Events from 'plug/core/Events'
+import RolloverEvent from 'plug/events/ShowUserRolloverEvent'
 
-  const UserListView = require('plug/views/rooms/users/RoomUsersListView')
-  const UserRowView = require('plug/views/rooms/users/RoomUserRowView')
-  const Events = require('plug/core/Events')
-  const RolloverEvent = require('plug/events/ShowUserRolloverEvent')
-  const { throttle } = require('underscore')
+class VoteRowView extends UserRowView {
+  onClick() {
+    Events.dispatch(new RolloverEvent(RolloverEvent.SHOW, this.model, {
+      x: this.$el.parents('.extplug-vote-list').offset().left - 5,
+      y: this.$el.offset().top + 1
+    }, true))
+  }
+}
 
-  const VoteRowView = UserRowView.extend({
-    onClick() {
-      Events.dispatch(new RolloverEvent(RolloverEvent.SHOW, this.model, {
-        x: this.$el.parents('.extplug-vote-list').offset().left - 5,
-        y: this.$el.offset().top + 1
-      }, true))
-    }
-  })
-
-  const VoteListView = UserListView.extend({
-    RowClass: VoteRowView,
-    initialize() {
-      this._super()
-      this.draw = throttle(this.draw, 120)
-      // TODO _probably_ unnecessary--FilteredCollection should deal with this?
-      this.collection.on('change:vote change:grab', this.draw, this)
-    },
-    remove() {
-      this.collection.off('change:vote change:grab', this.draw)
-    }
-  })
-
-  module.exports = VoteListView
-
+const VoteListView = UserListView.extend({
+  RowClass: VoteRowView,
+  initialize() {
+    this._super()
+    this.draw = throttle(this.draw, 120)
+    // TODO _probably_ unnecessary--FilteredCollection should deal with this?
+    this.collection.on('change:vote change:grab', this.draw, this)
+  },
+  remove() {
+    this.collection.off('change:vote change:grab', this.draw)
+  },
 })
+
+export default VoteListView
